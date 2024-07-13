f1ab2524eb927025128139dedff23340c735c39f
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-13 13:38:23 -0400

    adds flatpak XDG_DATA_DIRS cause lazy

--- files/etc/polaris/polaris-svc.sh
+++ files/etc/polaris/polaris-svc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# this shits a bit hacky
+# this stuffs a bit hacky
 
 /usr/bin/python /usr/share/polaris/polokeys_user.py
 /usr/bin/python /usr/share/polaris/default-hack.py
+
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
