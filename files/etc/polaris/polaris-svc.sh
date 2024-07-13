da10b002a5c1c36881df0acd44ce622286f7c75a
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-13 13:46:34 -0400

    rearrange which commands run when in the autostart

--- files/etc/polaris/polaris-svc.sh
+++ files/etc/polaris/polaris-svc.sh
@@ -2,7 +2,8 @@
 
 # this stuffs a bit hacky
 
-/usr/bin/python /usr/share/polaris/polokeys_user.py
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
+
 /usr/bin/python /usr/share/polaris/default-hack.py
 
-export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
+/usr/bin/python /usr/share/polaris/polokeys_user.py &
