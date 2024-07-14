6f5062c5c23fd70c852a0845dcade42f49fa43ac
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-14 15:46:18 -0400

    zramd + polo-adm secure wipe finished

--- src/polo-adm.py
+++ src/polo-adm.py
@@ -2,28 +2,46 @@
 import argparse
 import os
 import pcore
+import tqdm
 
 def get_device_size(device):
     statvfs = os.statvfs(device)
     return statvfs.f_blocks
 
 def secure_wipe(dev):
+    print("Removing the master boot record...")
+    os.system(f"dd if=/dev/zero of={dev} bs=446 count=1")
+    print("Wiping disk... (may take a while)")
     size = get_device_size(dev)
-    for _ in range(3):
-        dev_io = open(dev, "wb")
-        dev_io.write(os.urandom(size))
-        dev_io.close()
+    with tqdm.tqdm(range(size * 2), unit="chunk") as tq:
+        with open(dev, "wb") as dev_io:
+            for __ in range(size):
+                tq.update()
+                dev_io.write(os.urandom(1))
 
-    for _ in range(3):
-        dev_io = open(dev, "wb")
-        dev_io.write(b'\x00' * size)
-        dev_io.close()
+        with open(dev, "wb") as dev_io:
+            for __ in range(size):
+                tq.update()
+                dev_io.write(b'\x00')
 
+def diskcpy(src, out):
+    src_size = get_device_size(src)
+    out_size = get_device_size(out)
+    if src_size > out_size:
+        print("Error: Source device is bigger than output device")
+        print("Aborting")
+        return
+    with tqdm.tqdm(range(src_size * 2), unit="chunk") as tq:
+        with open(src, "rb") as src_io:
+            out_io = open(out, "wb+")
+            for __ in range(src_size):
+                tq.update()
+                src_io.write(out_io.read())
 
 def rebuild_boot():
     print("Rebuilding boot info")
-    os.system("sudo mkinitcpio -P")
-    os.system("sudo grub-mkconfig -o /boot/grub/grub.cfg")
+    os.system("mkinitcpio -P")
+    os.system("grub-mkconfig -o /boot/grub/grub.cfg")
 
 def banner():
     print(f"polo-adm | Polaris Polo Admin Tools | {pcore.VERSION}")
@@ -38,6 +56,11 @@ def main():
     secure_wipe_parser = subparsers.add_parser('secure-wipe', help='Securely overwrites the data of a disk, removing any trace of data. (may take a few hours!)')
     secure_wipe_parser.add_argument('disk', type=str, help='The disk to wipe')
 
+    # Disk Image command
+    disk_copy_parser = subparsers.add_parser('disk-cpy', help='Copys the content of a disk to a file/other disk.')
+    disk_copy_parser.add_argument('src', type=str, help='Source disk')
+    disk_copy_parser.add_argument('out', type=str, help='Output disk')
+
     # Rebuild-boot command
     rebuild_boot_parser = subparsers.add_parser('rebuild-boot', help='Rebuilds the GRUB config, as well as the initramfs.')
 
@@ -47,6 +70,8 @@ def main():
         rebuild_boot()
     elif args.command == "secure-wipe":
         secure_wipe(args.disk)
+    elif args.command == "disk-cpy":
+        diskcpy(args.src, args.out)
     else:
         parser.print_help()
 if __name__ == '__main__':
