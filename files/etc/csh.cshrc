d70ff43abc14b186dc514a7de6756d130599b200
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-20 18:48:52 -0400

    added tcsh, xonsh, elvish, fish and... nushell eventually?

---
+++ files/etc/csh.cshrc
@@ -0,0 +1,3 @@
+setenv EDITOR `python3 /usr/share/polaris/pcore-shack.py editor`
+setenv TERMINAL `python3 /usr/share/polaris/pcore-shack.py terminal`
+setenv BROWSER `python3 /usr/share/polaris/pcore-shack.py browser`
---
+++ files/etc/fish/config.fish
@@ -0,0 +1,3 @@
+set -x EDITOR (python3 /usr/share/polaris/pcore-shack.py editor)
+set -x TERMINAL (python3 /usr/share/polaris/pcore-shack.py terminal)
+set -x BROWSER (python3 /usr/share/polaris/pcore-shack.py browser)
