4db76d66de2083d97f40bcc316f22cb3b406d8f9
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-23 19:39:52 -0400

    remove sys.conf

--- src/pcore.py
+++ src/pcore.py
@@ -5,19 +5,12 @@ VERSION = 1.0
 
 # its config time (and then i configurated all over the place)
 
-def config():
-    configparser_ = configparser.ConfigParser()
-    configparser_.read("/etc/polaris/sys.conf")
-    return configparser_
-
 def config_usr():
     configparser_ = configparser.ConfigParser()
     configparser_.read(f"{os.path.expanduser("~")}/usr.conf")
     return configparser_
 
-cfg = config()
 cfgusr = config_usr()
-ostask = cfg["conf"]["task"]
 editor = cfgusr["conf"]["editor"]
 terminal = cfgusr["conf"]["terminal"]
 browser = cfgusr["conf"]["browser"]
