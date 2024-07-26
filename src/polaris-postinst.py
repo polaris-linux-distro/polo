e44e5974f451cbf141320b215387775bef303936
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-25 23:03:30 -0400

    i need a break....

--- src/polaris-postinst.py
+++ src/polaris-postinst.py
@@ -15,7 +15,7 @@ os.system("useradd -m -s /bin/zsh polarislinuxtempbuilder")
 os.system("usermod -aG wheel polarislinuxtempbuilder")
 os.system("echo 'y' | passwd polarislinuxtempbuilder -s")
 for pkg in aur_list:
-	os.system(f"echo y | sudo -u builder /usr/bin/python /usr/share/polaris/polo-pkg.py install {pkg} -s")
+	os.system(f"echo y | sudo -u polarislinuxtempbuilder /usr/bin/python /usr/share/polaris/polo-pkg.py install {pkg} -s")
 os.system("userdel -rf polarislinuxtempbuilder")
 
 os.system("systemctl disable polaris-postinst")
